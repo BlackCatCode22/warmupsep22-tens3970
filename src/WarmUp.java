71fcc1a80da2e9df2ba0b6549500a74c3912d98c
TobyEns <tens3970@gmail.com>
2022-09-20 10:59:25 -0700

    Added 1st advanced portion of the project and included a section to print the file's data

--- src/WarmUp.java
+++ src/WarmUp.java
@@ -9,7 +9,7 @@ public class WarmUp {
         try {
             File myFile = new File("C:/Users/Toby/Desktop/Programming/CIT Projects/WarmUp9-20/textFiles/test.txt");
             if (myFile.createNewFile()){
-                System.out.println("Pog, new file made.");
+                System.out.println("New file made.");
             } else {
                 System.out.println("Failed to create a new file.");
             }
@@ -19,7 +19,11 @@ public class WarmUp {
         }
         try {
             FileWriter Writer = new FileWriter("C:/Users/Toby/Desktop/Programming/CIT Projects/warmupsep22-tens3970/textFiles/test.txt");
-            Writer.write("This is the text written into the new file");
+            String textToWrite = "";
+            for (int i = 0; i < 100; i++){
+                textToWrite = textToWrite + "Line Numero:" + i + "\n";
+            }
+            Writer.write(textToWrite);
             Writer.close();
             System.out.println("Successfully Wrote into a new file.");
         }catch (IOException e){
@@ -33,9 +37,20 @@ public class WarmUp {
                 String data = obj.nextLine();
                 System.out.println(data);
             }
+            obj.close();
         } catch (IOException e){
             System.out.println("Error:");
             e.printStackTrace();
         }
+            File fileToExamine = new File("C:/Users/Toby/Desktop/Programming/CIT Projects/warmupsep22-tens3970/textFiles/test.txt");
+            if (fileToExamine.exists()){
+                System.out.println("File Name: " + fileToExamine.getName());
+                System.out.println("Absolute Path: " + fileToExamine.getAbsolutePath());
+                System.out.println("Writeable: " + fileToExamine.canWrite());
+                System.out.println("Readable: " + fileToExamine.canRead());
+                System.out.println("File Size in Bytes: " + fileToExamine.length());
+            } else {
+                System.out.println("File does not exist");
+            }
     }
 }
